d9c75ffc27c6c400e35516ea720bdd3fa5ef6477
Jared Stemen <jstemen@gmail.com>
2025-07-29 12:27:17 -0400

    Add AssertJ testing library for improved assertions

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,7 +10,8 @@ repositories {
 }
 
 val lombokVersion = "1.18.+"
-val mockitoVersion =  "5.11.+"
+val mockitoVersion = "5.11.+"
+val assertjVersion = "3.25.+" // Latest version of AssertJ
 
 dependencies {
     // Existing dependencies remain unchanged
@@ -24,15 +25,19 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:${lombokVersion}")
     testCompileOnly("org.projectlombok:lombok:${lombokVersion}")
     testAnnotationProcessor("org.projectlombok:lombok:${lombokVersion}")
+    
     // Mockito dependencies
     testImplementation("org.mockito:mockito-core:${mockitoVersion}")
     testImplementation("org.mockito:mockito-junit-jupiter:${mockitoVersion}")
-
+    
+    // AssertJ for fluent assertions
+    testImplementation("org.assertj:assertj-core:${assertjVersion}")
 }
 
 dependencyLocking {
     lockAllConfigurations()
 }
+
 // Add this to your existing build.gradle.kts
 tasks.test {
     useJUnitPlatform()
